518d281a10e92175665b327850fc0fc20b33f7ca
李学三 <xsli@g2link.cn>
2018-07-22 09:59:27 +0800

    learning

---
+++ InchToMeter.java
@@ -0,0 +1,16 @@
+class InchToMeter {
+    public static void main(String args[]) {
+	double inch = 1, meter;
+	int counter;
+
+	for(counter = 0; counter < 12 * 12; counter++) {
+	    meter = inch * 38.37;
+	    System.out.println(inch + " inches is " + meter + " meters");
+	    
+	    inch = inch + 1.0;
+	    if((counter + 1) % 12 == 0){
+		System.out.println();
+	    }
+	}
+    }
+}
---
+++ WeightOnMoon.java
@@ -0,0 +1,7 @@
+class WeightOnMoon {
+    public static void main(String args[]) {
+	double weight = 70;
+	double wom = weight * 0.17;
+	System.out.println("Weight " + weight + " on moon is " + wom);
+    }
+}
